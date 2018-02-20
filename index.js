baeba865f48019ed016db9508985e719667eec43
Nicolás Arias <nicolas@nicolasarias.com>
2018-02-21 00:42:04 +0700

    Update to statiq 0.5

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 const path = require('path');
 
-module.exports = function(statiq, options) {
+module.exports = function(options) {
+  return function(statiq) {
     const { contextHandler, indexHandler } = statiq._config;
     const { key } = Object.assign({ key: 'tags' }, options);
     const tags = {};
 
     statiq.config({
-        contextHandler: function(context, source, target) {
-            const ctx = contextHandler(context, source, target);
-            const ctxTags = Array.isArray(ctx[key]) ? ctx[key] : (ctx[key] || 'untagged').split(',');
+      contextHandler: function(context, source, target) {
+        const ctx = contextHandler(context, source, target);
+        const ctxTags = Array.isArray(ctx[key]) ? ctx[key] : (ctx[key] || 'untagged').split(',');
 
-            ctxTags.forEach(tag => {
-                if (!tags[tag]) tags[tag] = [];
-                tags[tag].push(Object.assign({}, context, { path: target }));
-            });
+        ctxTags.forEach(tag => {
+          tag = tag.trim();
+          if (!tags[tag]) tags[tag] = [];
+          tags[tag].push(Object.assign({}, context, { path: target }));
+        });
 
-            return ctx;
-        },
+        return ctx;
+      },
 
-        indexHandler: function(index, source, target) {
-            const idx = indexHandler(index, source, target);
-            idx[key] = {};
+      indexHandler: function(index, source, target) {
+        const idx = indexHandler(index, source, target);
+        idx[key] = {};
 
-            Object.keys(tags).forEach(tag => {
-                idx[key][tag] = tags[tag].map(ctx => {
-                    return Object.assign({}, ctx, { path: path.relative(path.dirname(target), ctx.path) });
-                });
-            });
+        Object.keys(tags).forEach(tag => {
+          idx[key][tag] = tags[tag].map(ctx => {
+            return Object.assign({}, ctx, { path: path.relative(path.dirname(target), ctx.path) });
+          });
+        });
 
-            return idx;
-        }
+        return idx;
+      }
     });
+  };
 }
